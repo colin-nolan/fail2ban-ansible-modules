280a05f508efdba0a7bf4d6260fc38daac5815b1
Colin Nolan <cn580@alumni.york.ac.uk>
2019-01-07 20:04:02 +0000

    Fixes issue with getting coverage in Travis CI.

--- run-tests-in-docker.sh
+++ run-tests-in-docker.sh
@@ -9,4 +9,4 @@ docker run --rm -it -v "${scriptDirectory}":/fail2ban-ansible-modules \
     fail2ban-ansible-modules-tests /fail2ban-ansible-modules/run-tests.sh
 
 # Hack the paths in the coverage report (unfortunately they are absolute)
-docker run --rm -it -v $PWD/:/data alpine sed -i -e "s|\"/patchworkdocker|\"${scriptDirectory}|g" /data/.coverage
+docker run --rm -it -v $PWD/:/data alpine sed -i -e "s|\"/fail2ban-ansible-modules|\"${scriptDirectory}|g" /data/.coverage
